e60b628bec578f9b8aae2821449919fce1f62699
jose.molina <jose.molina@nexcel.cl>
2021-11-09 18:50:27 -0300

    segundo comint

---
+++ pages/pokemones/[id].js
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+
+const Pokemon = ({data}) =>{
+    const router = useRouter()
+    if(router.isFallback){
+        return <p>Cargando...</p>
+    }
+    return(
+        <div>
+            <h1>{data.name} numero #{data.id}</h1>
+            <Image src={data.sprites.front_default} width={100} height={100}/>
+            <Link href="/" >Volver al inicio</Link>
+        </div>
+    )
+}
+
+export default Pokemon
+
+export const getStaticProps = async ({params}) =>{
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+    const data = await response.json()
+
+    return{
+        props: {data}
+    }
+}
+
+export const getStaticPaths = async () =>{
+
+    const paths = [
+        {params: {id : '1'}},
+    ]
+
+    return{
+        paths,
+        fallback: true
+    }
+} 
+
+// export const getServerSideProps = async ({params}) =>{
+
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+//     const data = await response.json()
+
+//     return{
+//         props: {data}
+//     }
+// }
